14e709914008c1ac226a3c1a318a9f55fd5f910d
Amos Wenger <amos@lockfree.ch>
2013-12-01 22:19:27 +0100

    Add one test for indent/ooc.vim and .travis.yml

---
+++ spec/indent_ooc_spec.rb
@@ -0,0 +1,28 @@
+
+require 'spec_helper'
+
+describe 'indent/ooc.vim' do
+  describe 'Basic block behavior' do
+    specify 'blocks such as classes, funcs, should be indented correctly' do
+      write_file('test.ooc', <<-EOF)
+        Dog: class {
+        init: func {
+        a: Int
+        }
+        } 
+      EOF
+
+      vim.edit 'test.ooc'
+      do_indent(vim)
+
+      IO.read('test.ooc').rstrip.should eq normalize_string_indent(<<-EOF)
+        Dog: class {
+            init: func {
+                a: Int
+            }
+        }
+      EOF
+    end
+  end
+end
+
