76c2c7e079041ca2601ff8c366054d670241dee3
Amos Wenger <amos@lockfree.ch>
2013-12-04 16:03:40 +0100

    better spec naming

--- spec/indent_ooc_spec.rb
+++ spec/indent_ooc_spec.rb
@@ -2,8 +2,9 @@
 require 'spec_helper'
 
 samples.each do |sample|
-  describe sample do
-    specify 'indentation should correspond to reference' do
+  simplename = File.basename(sample).gsub(/\.ooc$/, '')
+  describe simplename do
+    specify 'should indent' do
       write_file('test.ooc', IO.read(sample).rstrip)
       vim.edit 'test.ooc'
       vim.set('ft', 'ooc')
